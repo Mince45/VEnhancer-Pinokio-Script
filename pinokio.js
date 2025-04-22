577cf82275653c92fcc2a7a10d35f3534ba3bc27
Никишин Степан Павлович <nikishin_sp@taximaxim.local>
2025-04-22 13:27:09 +0500

    test1

--- pinokio.js
+++ pinokio.js
@@ -1,10 +1,23 @@
-// pinokio.js
+const path = require('path')
 module.exports = {
-  name: "VEnhancer",
+  version: "1.5",
+  title: "VEnhancer",
   description: "AI‑ускоритель видео на базе VChitect/VEnhancer",
   icon: "icon.png",
-  scriptVersion: "1.5",
-  install: "install.json",
-  start:   "start.json",
-  defaultRun: "start"
-};
+  menu: async (kernel) => {
+    const installing = await kernel.running(__dirname, "install.js")
+    const installed = await kernel.exists(__dirname, "app", "env")
+    const running = await kernel.running(__dirname, "start.js")
+    if (installing) {
+      return [ { title: "Installing…", selected: true, action: "install" } ]
+    } else if (!installed) {
+      return [ { title: "Install", selected: true, action: "install" } ]
+    } else {
+      return [
+        { title: "Start", selected: !running, action: "start" },
+        { title: "Stop", selected: running, action: "stop" },
+        { title: "Update", selected: false, action: "update" }
+      ]
+    }
+  }
+}
