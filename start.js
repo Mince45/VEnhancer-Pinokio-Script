f8fff96dff18fe493c1dbb8e604f23b97b994cbc
Никишин Степан Павлович <nikishin_sp@taximaxim.local>
2025-04-22 13:50:57 +0500

    test1

--- start.js
+++ start.js
@@ -1,13 +1,15 @@
+// start.js
 module.exports = async (kernel) => {
   return {
-    // не daemon — сразу отработает и закроется
     run: [
       {
         method: "shell.run",
         params: {
           venv: "env",
-          // запускаем главный скрипт из папки app
-          message: "python app/main.py --input {{input}} --output {{output}} --scale 2 --fps 60"
+          path: "app",
+          message: [
+            "python gradio_app.py --input \"{{input}}\" --output \"{{output}}\" --scale 2 --fps 60"
+          ]
         }
       }
     ]
