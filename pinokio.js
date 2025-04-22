16f2676e06b02d3a5aa6951c0331f30cb40c25b0
Никишин Степан Павлович <nikishin_sp@taximaxim.local>
2025-04-22 11:47:25 +0500

    test1

--- pinokio.js
+++ pinokio.js
@@ -1,10 +1,9 @@
-// pinokio.js
 module.exports = {
   name: "VEnhancer",
   description: "AI‑ускоритель видео на базе VChitect/VEnhancer",
-  icon: "icon.png",        // клади рядом файл icon.png (64×64px)
-  scriptVersion: "1.5",    // версия скриптов Pinokio (1.5+)
-  install: "install.json", // файл с командами установки
-  start: "start.json",     // файл с командами старта
-  defaultRun: "start"      // при клике “Run” будет выполнен start
+  icon: "icon.png",
+  scriptVersion: "1.5",
+  install: "install.json",
+  start: "start.json",
+  defaultRun: "start"
 };
